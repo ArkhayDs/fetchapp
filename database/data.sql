0c24fbc100fe744e2ee3dacf21a442d8d3bbc11d
ArkhayDs <raphael.ds.tic@gmail.com>
2022-04-10 21:38:38 +0200

    ajout dump.sql

--- database/data.sql
+++ database/data.sql
@@ -2,10 +2,10 @@
 -- version 5.1.3
 -- https://www.phpmyadmin.net/
 --
--- Hôte : db
--- Généré le : ven. 08 avr. 2022 à 13:16
--- Version du serveur : 10.6.5-MariaDB-1:10.6.5+maria~focal
--- Version de PHP : 8.0.16
+-- Host: db
+-- Generation Time: Apr 10, 2022 at 07:37 PM
+-- Server version: 10.7.3-MariaDB-1:10.7.3+maria~focal
+-- PHP Version: 8.0.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,88 +18,96 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `exampledb`
+-- Database: `data`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `post`
+-- Table structure for table `post`
 --
 
 CREATE TABLE `post` (
-                        `idPost` int(11) NOT NULL,
-                        `title` varchar(255) NOT NULL,
-                        `content` varchar(255) NOT NULL,
-                        `idUser` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `idPost` int(11) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Content` varchar(255) NOT NULL,
+  `idUser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `post`
+-- Dumping data for table `post`
 --
 
-INSERT INTO `post` (`idPost`, `title`, `content`, `idUser`) VALUES
-                                                                (1, 'fve', 'erfve', 1),
-                                                                (2, 'hello', 'ieijeijegijgijgigje', 2),
-                                                                (3, 'hello', 'ieijeijegijgijgigje', 3);
+INSERT INTO `post` (`idPost`, `Title`, `Content`, `idUser`) VALUES
+(1, 'fve', 'erfve', 6);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-                        `idUser` int(11) NOT NULL,
-                        `username` varchar(255) NOT NULL,
-                        `password` varchar(11) NOT NULL,
-                        `token` char(50) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `idUser` int(11) NOT NULL,
+  `Username` varchar(255) NOT NULL,
+  `Password` varchar(11) NOT NULL,
+  `Token` char(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`idUser`, `username`, `password`, `token`) VALUES
-                                                                   (1, 'amin', '1234', ''),
-                                                                   (2, 'basil', '1234', ''),
-                                                                   (3, 'exampleuser', '', ''),
-                                                                   (4, 'zidane', '12345', ''),
-                                                                   (5, 'antoine', '12345', 'aufuiefiuafiunfaiuzfn'),
-                                                                   (6, 'alpha', '12345', '04af1903b47dfcc195c8917dee27fbfd87f961e3b0cb84022a');
+INSERT INTO `user` (`idUser`, `Username`, `Password`, `Token`) VALUES
+(6, 'alpha', '12345', '04af1903b47dfcc195c8917dee27fbfd87f961e3b0cb84022a'),
+(16, 'topkek', 'cou', 'da71d33a41664287487b492d4a7269e8921fee2bc4f31d3e75'),
+(17, 'knee', 'coucou', 'bbf69b58c3a278c37d5039b8f05dc2d289e26a8b4e5f4b0e86'),
+(19, 'mlkj', 'lkjhlkjh', 'aa0f6527d678dad36876be0c5ab89b17fa5cb70d89c397579c');
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `post`
+-- Indexes for table `post`
 --
 ALTER TABLE `post`
-    ADD PRIMARY KEY (`idPost`),
+  ADD PRIMARY KEY (`idPost`),
   ADD KEY `idUser` (`idUser`);
 
 --
--- Index pour la table `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
-    ADD PRIMARY KEY (`idUser`);
+  ADD PRIMARY KEY (`idUser`),
+  ADD UNIQUE KEY `idUser` (`idUser`),
+  ADD UNIQUE KEY `Username` (`Username`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `post`
+-- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-    MODIFY `idPost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idPost` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT pour la table `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-    MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `idUser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `post_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `user` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
